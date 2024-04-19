54185b4b0f65eaf407c59f4e46f4799a3b1b6bd6
atik.kn <atik.kn@arista.com>
2024-04-19 11:10:19 +0530

    maintainers visible

--- src/extension.ts
+++ src/extension.ts
@@ -5,111 +5,111 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 
-export class NodeDependenciesProvider implements vscode.TreeDataProvider<Dependency> {
-  constructor(private workspaceRoot: string) {}
+// export class NodeDependenciesProvider implements vscode.TreeDataProvider<Dependency> {
+//   constructor(private workspaceRoot: string) {}
 
-  getTreeItem(element: Dependency): vscode.TreeItem {
-    return element;
-  }
+//   getTreeItem(element: Dependency): vscode.TreeItem {
+//     return element;
+//   }
 
-  getChildren(element?: Dependency): Thenable<Dependency[]> {
-    if (!this.workspaceRoot) {
-      vscode.window.showInformationMessage('No dependency in empty workspace');
-      return Promise.resolve([]);
-    }
+//   getChildren(element?: Dependency): Thenable<Dependency[]> {
+//     if (!this.workspaceRoot) {
+//       vscode.window.showInformationMessage('No dependency in empty workspace');
+//       return Promise.resolve([]);
+//     }
 
-    if (element) {
-      return Promise.resolve(
-        this.getDepsInPackageJson(
-          path.join(this.workspaceRoot, 'node_modules', element.label, 'package.json')
-        )
-      );
-    } else {
-      const packageJsonPath = path.join(this.workspaceRoot, 'package.json');
-      if (this.pathExists(packageJsonPath)) {
-        return Promise.resolve(this.getDepsInPackageJson(packageJsonPath));
-      } else {
-        vscode.window.showInformationMessage('Workspace has no package.json');
-        return Promise.resolve([]);
-      }
-    }
-  }
+//     if (element) {
+//       return Promise.resolve(
+//         this.getDepsInPackageJson(
+//           path.join(this.workspaceRoot, 'node_modules', element.label, 'package.json')
+//         )
+//       );
+//     } else {
+//       const packageJsonPath = path.join(this.workspaceRoot, 'package.json');
+//       if (this.pathExists(packageJsonPath)) {
+//         return Promise.resolve(this.getDepsInPackageJson(packageJsonPath));
+//       } else {
+//         vscode.window.showInformationMessage('Workspace has no package.json');
+//         return Promise.resolve([]);
+//       }
+//     }
+//   }
 
-  /**
-   * Given the path to package.json, read all its dependencies and devDependencies.
-   */
-  private getDepsInPackageJson(packageJsonPath: string): Dependency[] {
-    if (this.pathExists(packageJsonPath)) {
-      const toDep = (moduleName: string, version: string): Dependency => {
-        if (this.pathExists(path.join(this.workspaceRoot, 'node_modules', moduleName))) {
-          return new Dependency(
-            moduleName,
-            version,
-            vscode.TreeItemCollapsibleState.Collapsed
-          );
-        } else {
-          return new Dependency(moduleName, version, vscode.TreeItemCollapsibleState.None);
-        }
-      };
+//   /**
+//    * Given the path to package.json, read all its dependencies and devDependencies.
+//    */
+//   private getDepsInPackageJson(packageJsonPath: string): Dependency[] {
+//     if (this.pathExists(packageJsonPath)) {
+//       const toDep = (moduleName: string, version: string): Dependency => {
+//         if (this.pathExists(path.join(this.workspaceRoot, 'node_modules', moduleName))) {
+//           return new Dependency(
+//             moduleName,
+//             version,
+//             vscode.TreeItemCollapsibleState.Collapsed
+//           );
+//         } else {
+//           return new Dependency(moduleName, version, vscode.TreeItemCollapsibleState.None);
+//         }
+//       };
 
-      const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+//       const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 
-      const deps = packageJson.dependencies
-        ? Object.keys(packageJson.dependencies).map(dep =>
-            toDep(dep, packageJson.dependencies[dep])
-          )
-        : [];
-      const devDeps = packageJson.devDependencies
-        ? Object.keys(packageJson.devDependencies).map(dep =>
-            toDep(dep, packageJson.devDependencies[dep])
-          )
-        : [];
-      return deps.concat(devDeps);
-    } else {
-      return [];
-    }
-  }
+//       const deps = packageJson.dependencies
+//         ? Object.keys(packageJson.dependencies).map(dep =>
+//             toDep(dep, packageJson.dependencies[dep])
+//           )
+//         : [];
+//       const devDeps = packageJson.devDependencies
+//         ? Object.keys(packageJson.devDependencies).map(dep =>
+//             toDep(dep, packageJson.devDependencies[dep])
+//           )
+//         : [];
+//       return deps.concat(devDeps);
+//     } else {
+//       return [];
+//     }
+//   }
 
-  private pathExists(p: string): boolean {
-    try {
-      fs.accessSync(p);
-    } catch (err) {
-      return false;
-    }
-    return true;
-  }
-}
+//   private pathExists(p: string): boolean {
+//     try {
+//       fs.accessSync(p);
+//     } catch (err) {
+//       return false;
+//     }
+//     return true;
+//   }
+// }
 
-class Dependency extends vscode.TreeItem {
-  constructor(
-    public readonly label: string,
-    private version: string,
-    public readonly collapsibleState: vscode.TreeItemCollapsibleState
-  ) {
-    super(label, collapsibleState);
-    this.tooltip = `${this.label}-${this.version}`;
-    this.description = this.version;
-  }
+// class Dependency extends vscode.TreeItem {
+//   constructor(
+//     public readonly label: string,
+//     private version: string,
+//     public readonly collapsibleState: vscode.TreeItemCollapsibleState
+//   ) {
+//     super(label, collapsibleState);
+//     this.tooltip = `${this.label}-${this.version}`;
+//     this.description = this.version;
+//   }
 
-  iconPath = {
-    light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
-    dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
-  };
-}
+//   iconPath = {
+//     light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+//     dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+//   };
+// }
 
-const rootPath =
-  vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
-    ? vscode.workspace.workspaceFolders[0].uri.fsPath
-    : '';
+// const rootPath =
+//   vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
+//     ? vscode.workspace.workspaceFolders[0].uri.fsPath
+//     : '';
 
-vscode.window.registerTreeDataProvider(
-  'nodeDependencies',
-  new NodeDependenciesProvider(rootPath)
-);
+// vscode.window.registerTreeDataProvider(
+//   'nodeDependencies',
+//   new NodeDependenciesProvider(rootPath)
+// );
 
-vscode.window.createTreeView('nodeDependencies', {
-	treeDataProvider: new NodeDependenciesProvider(rootPath)
-});
+// vscode.window.createTreeView('nodeDependencies', {
+// 	treeDataProvider: new NodeDependenciesProvider(rootPath)
+// });
 
 export class MaintainerProvider implements vscode.TreeDataProvider<Maintainer> {
 	constructor(private workspaceRoot: string) {}
@@ -120,24 +120,25 @@ export class MaintainerProvider implements vscode.TreeDataProvider<Maintainer> {
 
 	getChildren(element?: Maintainer): Thenable<Maintainer[]> {
 		if (!this.workspaceRoot) {
-		vscode.window.showInformationMessage('No dependency in empty workspace');
-		return Promise.resolve([]);
+			vscode.window.showInformationMessage('No dependency in empty workspace');
+			return Promise.resolve([]);
 		}
 
 		if (element) {
-		return Promise.resolve(
-			this.getMaintainers(
-			path.join(this.workspaceRoot, 'maintainers.json')
-			)
-		);
+			return Promise.resolve(
+				this.getMaintainers(
+					path.join(this.workspaceRoot, 'maintainers.json')
+				)
+			);
 		} else {
-		const maintainersFilePath = path.join(this.workspaceRoot, 'maintainers.json');
-		if (this.pathExists(maintainersFilePath)) {
-			return Promise.resolve(this.getMaintainers(maintainersFilePath));
-		} else {
-			vscode.window.showInformationMessage('Workspace has no maintainers.json');
-			return Promise.resolve([]);
-		}
+			const maintainersFilePath = path.join(this.workspaceRoot, 'maintainers.json');
+			if (this.pathExists(maintainersFilePath)) {
+				// vscode.window.showInformationMessage(maintainersFilePath);
+				return Promise.resolve(this.getMaintainers(maintainersFilePath));
+			} else {
+				vscode.window.showInformationMessage('Workspace has no maintainers.json');
+				return Promise.resolve([]);
+			}
 		}
 	}
 
@@ -146,22 +147,21 @@ export class MaintainerProvider implements vscode.TreeDataProvider<Maintainer> {
 	 */
 	private getMaintainers(maintainersFilePath: string): Maintainer[] {
 		if (this.pathExists(maintainersFilePath)) {
-			const toAlias = (alias: string): Maintainer => {
-				if (this.pathExists(path.join(this.workspaceRoot, 'node_modules', alias))) {
-				return new Maintainer(
-					alias,
-					vscode.TreeItemCollapsibleState.Collapsed
-				);
-				} else {
-				return new Maintainer(alias, vscode.TreeItemCollapsibleState.None);
-				}
-			};
-
-			const filePath = JSON.parse(fs.readFileSync(maintainersFilePath, 'utf-8'));
-
-			const maintainers = filePath.maintainers;
-			maintainers.concat(filePath.leadMaintainer);
-			maintainers.concat(filePath.watchers);
+			// return [new Maintainer("atik.kn")];
+			const maintainersJSONObject = JSON.parse(fs.readFileSync(maintainersFilePath, 'utf-8'));
+			const maintainers = maintainersJSONObject.maintainers ? 
+							maintainersJSONObject.maintainers.map((maintainer: string) => {return new Maintainer(maintainer);})
+							: [] ;
+			const leadMaintainer = maintainersJSONObject.leadMaintainer ?
+							maintainersJSONObject.leadMaintainer.map((lead: string) => {return new Maintainer(lead);}) 
+							: [];
+			const watchers = maintainersJSONObject.watchers ?
+							maintainersJSONObject.watchers.map((watcher: string) => {return new Maintainer(watcher);}) 
+							: [];
+			maintainers.concat(leadMaintainer,watchers);
+			maintainers.forEach(function(entry:any) {
+				console.log(entry);
+			});
 			return maintainers;
 		} else {
 			return [];
@@ -170,9 +170,9 @@ export class MaintainerProvider implements vscode.TreeDataProvider<Maintainer> {
 
 	private pathExists(p: string): boolean {
 		try {
-		fs.accessSync(p);
+			fs.accessSync(p);
 		} catch (err) {
-		return false;
+			return false;
 		}
 		return true;
 	}
@@ -181,18 +181,30 @@ export class MaintainerProvider implements vscode.TreeDataProvider<Maintainer> {
 class Maintainer extends vscode.TreeItem {
 	constructor(
 		public readonly alias: string,
-		public readonly collapsibleState: vscode.TreeItemCollapsibleState
-	  ) {
-		super(alias, collapsibleState);
+	) {
+		super(alias,vscode.TreeItemCollapsibleState.None);
 		this.tooltip = `${this.alias}`;
-	  }
-	
-	  iconPath = {
+	}
+
+	iconPath = {
 		light: path.join(__filename, '..', '..', 'resources', 'light', 'media/user.svg'),
 		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'media/user.svg')
-	  };
+	};
 }
 
+const workspaceRootPath =
+  vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
+    ? vscode.workspace.workspaceFolders[0].uri.fsPath
+    : '/Users/atik.kn/src/fingerTips/fingertips/';
+
+vscode.window.registerTreeDataProvider(
+  'fingertips',
+  new MaintainerProvider(workspaceRootPath)
+);
+
+vscode.window.createTreeView('fingertips', {
+	treeDataProvider: new MaintainerProvider(workspaceRootPath)
+});
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
